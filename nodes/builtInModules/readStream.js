2102081971b7f686aa776a09efabc6f43a7ac3d5
webfaisalbd <web.faisal.bd@gmail.com>
2022-07-24 22:55:00 +0600

    createReadStream and createWritableStream together

--- nodes/builtInModules/readStream.js
+++ nodes/builtInModules/readStream.js
@@ -3,15 +3,19 @@ const log = console.log;
 
 const fileName = './streams/resume.pdf';
 const readableStream = fs.createReadStream(fileName, { highWaterMark: 1024 });
+const writableStream = fs.createWriteStream('./streams/resume2.pdf');
 
 let i = 0;
 
 readableStream.on('data', (chunk) => {
     log(++i, '\t', chunk.length);
+    // write the readable chunk in writableStream
+    writableStream.write(chunk);
 
 });
 
 readableStream.on("end", () => {
     log('end');
+    writableStream.end();
 });
 
